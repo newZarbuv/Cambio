1e3877dd28fa2a7defe234bfeaf2c7711b3679e4
newZarbuv <31135691+newZarbuv@users.noreply.github.com>
2017-10-09 11:16:21 +0300

    fixing settins screen

--- app/src/main/java/com/example/a12/cambioproject/gameSettings.java
+++ app/src/main/java/com/example/a12/cambioproject/gameSettings.java
@@ -10,10 +10,7 @@ import java.security.PrivateKey;
 
 public class gameSettings extends AppCompatActivity {
 
-    private SeekBar seekPlayers;
-    private SeekBar seekLevel;
-    private TextView txtPlayers;
-    private TextView txtLevel;
+
     private static int LEVEL_MIN_VALUE=3;
     private static int LEVEL_MAX_VALUE=5;
     private static int PLAYERS_MIN_VALUE=2;
@@ -23,73 +20,23 @@ public class gameSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_settings);
-        NumberPicker num=(NumberPicker)findViewById(R.id.numPicLevel);
-        num.setMinValue(3);
-        num.setMaxValue(5);
-        initializeVariables();
-        txtPlayers.setText(Integer.toString(seekPlayers.getProgress()));
-        txtLevel.setText(Integer.toString(seekLevel.getProgress()));
-         seekLevel.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-               int progress = 3;
+        //initializing cambio level number picker
+        NumberPicker nLevel=(NumberPicker)findViewById(R.id.numPicLevel);
+        nLevel.setMinValue(LEVEL_MIN_VALUE);
+        nLevel.setMaxValue(LEVEL_MAX_VALUE);
+        //initializing number of players number picker
+        NumberPicker nPlayers=(NumberPicker)findViewById(R.id.numPicPlayers);
+        nPlayers.setMinValue(PLAYERS_MIN_VALUE);
+        nPlayers.setMaxValue(PLAYERS_MAX_VALUE);
 
 
-               @Override
-               public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                   if(i<=LEVEL_MIN_VALUE){
-                       progress=LEVEL_MIN_VALUE;
-                   }
-                   else {
-                       progress = i;
-                   }
-               }
-
-
-             @Override
-             public void onStartTrackingTouch(SeekBar seekBar) {
-
-             }
-
-            @Override
-             public void onStopTrackingTouch(SeekBar seekBar) {
-                 txtLevel.setText(Integer.toString(seekLevel.getProgress()));
-              }
-           });
     }
 
 
 
 
 
-      //  seekPlayers.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-     //       int progress = 0;
 
 
-      //      @Override
-     //       public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-      //          progress=i;
 
-      //      }
-
-      //      @Override
-      //      public void onStartTrackingTouch(SeekBar seekBar) {
-
-       //     }
-
-       //     @Override
-       //     public void onStopTrackingTouch(SeekBar seekBar) {
-       //         txtPlayers.setText(seekLevel.getProgress());
-     //       }
-      //  });
-  //  }
-
-
-    private void initializeVariables(){
-        seekPlayers=(SeekBar)findViewById(R.id.seekNum);
-        seekLevel=(SeekBar)findViewById(R.id.seekLevel);
-        txtLevel=(TextView)findViewById(R.id.txtShowLevel);
-        txtPlayers=(TextView)findViewById(R.id.txtShowNum);
-        seekPlayers.setMax(PLAYERS_MAX_VALUE);
-        seekLevel.setMax(LEVEL_MAX_VALUE);
-
-    }
 }
