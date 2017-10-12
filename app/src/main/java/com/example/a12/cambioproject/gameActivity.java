551fff25b1e70e7126a3d7ec1c908e7575fa49c8
newZarbuv <31135691+newZarbuv@users.noreply.github.com>
2017-10-12 20:54:31 +0300

    table and players in game activity

--- app/src/main/java/com/example/a12/cambioproject/gameActivity.java
+++ app/src/main/java/com/example/a12/cambioproject/gameActivity.java
@@ -6,9 +6,11 @@ import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class gameActivity extends AppCompatActivity {
 
@@ -17,19 +19,12 @@ public class gameActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        //create full screen window
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        //adapt the image to the size of the display
-        Display disp=getWindowManager().getDefaultDisplay();
-        Point size=new Point();
-        disp.getSize(size);
-        Bitmap bmp=Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
-                getResources(),R.mipmap.game_background),size.x,size.y,true);
-        //fill the image view with the bmp
+       //create full screen window
         ImageView iv=(ImageView)findViewById(R.id.iv_back);
-        iv.setImageBitmap(bmp);
+        iv.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        iv.setScaleType(ImageView.ScaleType.FIT_XY);
+
+
+
     }
 }
