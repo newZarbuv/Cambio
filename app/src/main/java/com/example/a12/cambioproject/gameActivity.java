ade580ac26e9b9de8a223e5d88236abc51b5e96c
newZarbuv <31135691+newZarbuv@users.noreply.github.com>
2017-10-14 15:30:03 +0300

    set players on table according to user selections

--- app/src/main/java/com/example/a12/cambioproject/gameActivity.java
+++ app/src/main/java/com/example/a12/cambioproject/gameActivity.java
@@ -3,6 +3,7 @@ package com.example.a12.cambioproject;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
@@ -11,20 +12,54 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.content.Intent;
 
 public class gameActivity extends AppCompatActivity {
+
+    private ImageView iv_Plr1;
+    private ImageView iv_Plr2;
+    private ImageView iv_Plr3;
+    private ImageView iv_Plr4;
+    private ImageView iv_Plr5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-       //create full screen window
-        ImageView iv=(ImageView)findViewById(R.id.iv_back);
-        iv.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
-        iv.setScaleType(ImageView.ScaleType.FIT_XY);
+        initializeVariables();
+        //get intent from settings activity and configure players
+          Intent settIntent=getIntent();
+        int playerNum=settIntent.getIntExtra("PLAYERS",2);
+        switch(playerNum){
+            case 2:
+                iv_Plr3.setVisibility(View.GONE);
+                iv_Plr4.setVisibility(View.GONE);
+                iv_Plr5.setVisibility(View.GONE);
+                break;
+
+            case 3:
+                iv_Plr3.setVisibility(View.GONE);
+                iv_Plr4.setVisibility(View.GONE);
+                break;
+
+            case 4:
+                iv_Plr5.setVisibility(View.GONE);
+                break;
+
+            default:
+                break;
 
 
+        }
+  }
 
+    private void initializeVariables(){
+        iv_Plr1=(ImageView)findViewById(R.id.iv_player1);
+        iv_Plr2=(ImageView)findViewById(R.id.iv_player2);
+        iv_Plr3=(ImageView)findViewById(R.id.iv_player3);
+        iv_Plr4=(ImageView)findViewById(R.id.iv_player4);
+        iv_Plr5=(ImageView)findViewById(R.id.iv_player5);
     }
 }
